ffae15cb3787c37ce3f07fe39476d4711863e1ee
Oniqued <zktl3575@gmail.com>
2023-01-19 22:46:52 +0900

    [정렬] 세 수

---
+++ src/Sorting/ThreeNums/Main.java
@@ -0,0 +1,31 @@
+package Sorting.ThreeNums;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int[] arr = new int[3];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for(int j = i+1; j < arr.length; j++){
+                if (arr[i] > arr[j]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+        bw.write(String.valueOf(arr[1]));
+
+        bw.flush();
+        bw.close();
+    }
+}
